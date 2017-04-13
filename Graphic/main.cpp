b3274181cc45fe5916d7315a5019195e37da473a
TU YI <niklaus_yi@tudemacbook-pro.fios-router.home>
2017-04-13 12:56:05 -0400

    Initial Commit

---
+++ Graphic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphic
+//
+//  Created by TU YI on 2017/4/13.
+//  Copyright © 2017年 TU YI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
